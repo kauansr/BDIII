1684abc50158e98f1c918cacd24f3baa23c9cae1
Kauan Silva <91165615+kauansr@users.noreply.github.com>
2022-08-30 22:07:06 -0300

    Add files via upload

---
+++ trigger.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION trgValidaDadosConsulta() returns trigger as $trgValidaDadosConsulta$;
+
+declare
+pac_row record;
+espec_row record;
+
+begin
+    RAISE NOTICE 'Meu trigger rodou!!!'
+    RETURN NEW;
+
+END;
+$trgValidaDadosConsulta$ LANGUAGE plpgsql;
+
+create TRIGGER trgValidaDadosConsulta
+BEFORE INSERT OR UPDATE ON consultas
+FOR EACH ROW 
+EXECUTE PROCEDURE trgValidaDadosConsulta();
